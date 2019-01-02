287665eb8b929579cfd346c937b663da6b8aacda
zhili.wzl <zhili.wzl@alibaba-inc.com>
2019-01-02 21:50:11 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ const toArr = val => (Array.isArray(val) ? val : val ? [val] : [])
 
 const ReactDocRenderer = ({ docs }) => {
   let max = docs.length + 1000
-  const sortedDocs = toArr(docs).sort(
-    ({ meta: meta_after = {} }, { meta: meta_prev = {} }) => {
+  const sortedDocs = toArr(docs)
+    .map(item => {
+      item.index = item.index || item.order
+      return item
+    })
+    .sort(({ meta: meta_after = {} }, { meta: meta_prev = {} }) => {
       if (meta_after.index && meta_prev.index) {
         return meta_after.index - meta_prev.index
       } else {
@@ -22,8 +26,7 @@ const ReactDocRenderer = ({ docs }) => {
         }
         return meta_after.index - meta_prev.index
       }
-    }
-  )
+    })
   return (
     <MenuContent>
       <div className="doc-renderer markdown-body" style={{ margin: 30 }}>
