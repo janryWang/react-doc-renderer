e86f253e0f0cf510547e1df56011149066bda54c
zhili.wzl <zhili.wzl@alibaba-inc.com>
2019-01-15 22:02:31 +0800

    update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const ReactDocRenderer = ({ docs }) => {
   let max = docs.length + 1000
   const sortedDocs = toArr(docs)
     .map(item => {
-      item.index = item.index || item.order
+      item.meta.index =
+        item.meta.index !== undefined ? item.meta.index : item.meta.order
       return item
     })
     .sort(({ meta: meta_after = {} }, { meta: meta_prev = {} }) => {
